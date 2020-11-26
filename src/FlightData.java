b1c0b74fb29be0d801ba26bcd75547e97878ada6
aneeshp10 <56106124+aneeshp10@users.noreply.github.com>
2020-11-27 00:14:59 +0530

    Update FlightData.java

--- src/FlightData.java
+++ src/FlightData.java
@@ -1,11 +1,3 @@
-// --== CS400 File Header Information ==--
-// Name: Aneesh Patil
-// Email: apatil6@wisc.edu
-// Team: GA
-// TA: Daniel Kiel
-// Lecturer: Gary Dahl
-// Notes to Grader: None
-
 import java.util.PriorityQueue;
 
 /**
